e5ba0c6a1f00bf6481333eed0604791914cd2437
bokhyunnsoo <hsbok120@gmail.com>
2020-05-27 22:53:19 +0900

    Junit Test, Rest Api, Restaurant List

---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -0,0 +1,25 @@
+package kr.co.fastcampus.eatgo.domain;
+
+public class Restaurant {
+    private final long id;
+    private final String name;
+    private final String address;
+
+    public Restaurant(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
